dddbcb7ee20726f932089dc943c8fc8f66cfab1b
tamminen.mikkoj@gmail.com <tamminen.mikkoj@gmail.com>
2018-09-05 16:11:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.lowercase()===string)
+  if (string.lowercase()===string) {
+    return "I can't hear you!"
 }
