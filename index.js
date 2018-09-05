bd52951f9eb2e9ede49f3de499a53b7dee8b93fd
tamminen.mikkoj@gmail.com <tamminen.mikkoj@gmail.com>
2018-09-05 16:05:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,29 +10,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(`string`.toLowerCase)
 }
-function sayHiToGrandma(string) {
-  return "I can't hear you!" if string === lowercase
-}
-
-function sayHiToGrandma(string) {
-  return "YES INDEED!" if string === uppercase
-}
-
-function sayHiToGrandma(string) {
-  return "I love you, too." if string === "I love you, Grandma."
-}
-
-
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
